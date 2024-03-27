a908b93470e4881b4a293bba48495da249f8e96c
IgoorDrt <igor.2006duarte@gmail.com>
2024-03-27 14:57:56 -0300

    atualizando tempo

--- src/screens/TempoScreenAula.jsx
+++ src/screens/TempoScreenAula.jsx
@@ -1,43 +1,71 @@
+import { Image } from "expo-image";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import styles from "../config/styles";
 
-const API_KEY = "547210dcda1c34b8926ea3960324bcb7"; //peguem a de vocês
-const CITY_NAME = "Joinville"; //peguem a de vocês
+/**
+ *
+ * LOGICA SIMPLES
+ * - ENTREI NESSA TELA
+ * - OPA! PRECISO BUSCAR A TEMPERATURA
+ * - COMO? QUEM É O CARA QUE EXECUTA QUANDO ENTRA?
+ * - LEMBREI É O U... COM [] VAZIO
+ * -
+ */
+const API = "547210dcda1c34b8926ea3960324bcb7";
 
-export default function TempoScreenAula(){
-    const [temperatura, setTemperatura] = useState('');
+export default function TempoScreenAula() {
+  const [temperatura, setTemperatura] = useState("");
+  const [icone, setIcone] = useState("");
+  const [cidade, setCidade] = useState("Joinville");
 
-    useEffect(() =>{
-        const fetchTempo = async () => {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`;
-      
-            // Faça a requisição usando Axios
-            try {
-              const response = await axios.get(url);
-              console.log(response.data);
-              // // A resposta está disponível no objeto response.data
-      
-              setTempoData(response.data);
-            } catch (error) {
-              // Trate o erro
-              console.error("There was an error!", error);
-            }
-          };
-          fetchTempo();
-    }, []);
+  const fetchTempo = async () => {
 
-    return(
-        <View style={styles.container}>
-      <Text variant="bodyLarge">Tempo em {CITY_NAME}</Text>
-      {tempoData && (
-        <Card style={styles.card}>
-          <Card.Title title="Detalhes do Tempo" />
-          <Card.Content>
-            <Text>Temperatura: {temperatura.main.temp}°C</Text>
-          </Card.Content>
-        </Card>
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API}&units=metric`;
+    // vou lá buscar o JSON na internet
+    const resposta = await fetch(URL);
+    // recebo essa informação e converto ela em programação que é JSON
+    const data = await resposta.json();
+    console.log(resposta); // formato texto
+    console.log(data); // já está convertido pra JSON
+    setTemperatura(data);
+    setIcone(data.weather[0].icon);
+  };
+
+  useEffect(() => {
+    fetchTempo();
+  }, []);
+  // array vazio no final de useEffect simula simboliza
+  // que a função será executada apenas uma vez, quando o componente for montado
+
+  return (
+    <View style={styles.container}>
+      {icone && (
+        <>
+          <Text
+            variant="displayMedium"
+            style={{ textAlign: "center", marginVertical: 10 }}
+          >
+            Temperatura em Joinville
+          </Text>
+          <Image
+            source={{
+              uri: `https://openweathermap.org/img/wn/${icone}@2x.png`,
+            }}
+            style={{
+              width: 100,
+              height: 100,
+              backgroundColor: "white",
+              borderRadius: 200,
+            }}
+          />
+        </>
       )}
+      <Text variant="headlineSmall">Informações</Text>
+      <Text>Temperatura atual: {temperatura?.main?.temp}</Text>
+      <Text>Temperatura Máxima: {temperatura?.main?.temp_max}</Text>
+      <Text>Temperatura Minima: {temperatura?.main?.temp_min}</Text>
     </View>
-    )
+  );
 }
