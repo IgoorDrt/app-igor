6514e913cd3f420317067eb94b64d5828d09e69a
IgoorDrt <igor.2006duarte@gmail.com>
2024-03-28 14:26:33 -0300

    atualização

--- src/screens/TempoScreenAula.jsx
+++ src/screens/TempoScreenAula.jsx
@@ -1,52 +1,44 @@
 import { Image } from "expo-image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { TextInput, Button, Text } from "react-native-paper";
 import styles from "../config/styles";
-
-/**
- *
- * LOGICA SIMPLES
- * - ENTREI NESSA TELA
- * - OPA! PRECISO BUSCAR A TEMPERATURA
- * - COMO? QUEM É O CARA QUE EXECUTA QUANDO ENTRA?
- * - LEMBREI É O U... COM [] VAZIO
- * -
- */
-const Cidade = "Berlin";
-const API = "547210dcda1c34b8926ea3960324bcb7";
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=${Cidade}&appid=${API}&units=metric`;
 
 export default function TempoScreenAula() {
   const [temperatura, setTemperatura] = useState("");
   const [icone, setIcone] = useState("");
+  const [cidade, setCidade] = useState("");
 
   const fetchTempo = async () => {
-    // vou lá buscar o JSON na internet
-    const resposta = await fetch(URL);
-    // recebo essa informação e converto ela em programação que é JSON
-    const data = await resposta.json();
-    console.log(resposta); // formato texto
-    console.log(data); // já está convertido pra JSON
-    setTemperatura(data);
-    setIcone(data.weather[0].icon);
-  };
+    const API = "547210dcda1c34b8926ea3960324bcb7";
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API}&units=metric`;
 
-  useEffect(() => {
-    fetchTempo();
-  }, []);
-  // array vazio no final de useEffect simula simboliza
-  // que a função será executada apenas uma vez, quando o componente for montado
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setTemperatura(data);
+      setIcone(data.weather[0].icon);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <TextInput
+        label="Nome da Cidade"
+        value={cidade}
+        onChangeText={text => setCidade(text)}
+        style={{ marginBottom: 10 }}
+      />
+      <Button onPress={fetchTempo}>Pesquisar</Button>
       {icone && (
         <>
           <Text
             variant="displayMedium"
             style={{ textAlign: "center", marginVertical: 10 }}
           >
-            Temperatura em {Cidade}
+            Temperatura em {cidade}
           </Text>
           <Image
             source={{
@@ -61,10 +53,23 @@ export default function TempoScreenAula() {
           />
         </>
       )}
-      <Text variant="headlineSmall">Informações</Text>
-      <Text>Temperatura atual: {temperatura?.main?.temp}</Text>
-      <Text>Temperatura Máxima: {temperatura?.main?.temp_max}</Text>
-      <Text>Temperatura Minima: {temperatura?.main?.temp_min}</Text>
+      {temperatura && (
+        <View>
+          <Text variant="headlineSmall" style={{ marginTop: 20 }}>
+            Informações
+          </Text>
+          <Text>Temperatura: {temperatura?.main?.temp}°C</Text>
+          <Text>
+            Sensação Térmica: {temperatura?.main?.feels_like}°C
+          </Text>
+          <Text>
+            Temperatura Máxima: {temperatura?.main?.temp_max}°C
+          </Text>
+          <Text>
+            Temperatura Mínima: {temperatura?.main?.temp_min}°C
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
